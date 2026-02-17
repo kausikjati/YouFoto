681185a8d9d35f146d1cbeaeaece67ea1abbeadf
Kausik Jati <kausikjati@gmail.com>
2026-02-17 19:27:28 +0530

    Initial Commit

---
+++ YouFoto/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  YouFoto
+//
+//  Created by Kausik Jati on 17/02/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ YouFotoUITests/YouFotoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  YouFotoUITestsLaunchTests.swift
+//  YouFotoUITests
+//
+//  Created by Kausik Jati on 17/02/26.
+//
+
+import XCTest
+
+final class YouFotoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
